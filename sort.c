cb965d9e7b02abac1f1c5ee9e83aecc852d1d7b1
Krage56 <ivan.proskuryakov.2001@yandex.ru>
2023-10-08 11:27:47 +0300

    пример с неверифицируемым поведением

--- sort.c
+++ sort.c
@@ -1,7 +1,6 @@
 // 3
 #define UPPER_LIMIT 255
 
-
 /*@ predicate Sorted(int *a, integer n) =
 @   \forall integer i;
 @   0 <= i < n - 1 ==> a[i] <= a[i + 1];
@@ -21,7 +20,7 @@ void count_pos(int *arr, int n) {
     int count[UPPER_LIMIT + 1];
     int i, j;
     /*@
-        @ loop variant UPPER_LIMIT - i;
+    @ loop variant UPPER_LIMIT - i;
     @*/
     for (i = 0; i <= UPPER_LIMIT; ++i) {
         count[i] = 0;
@@ -35,10 +34,14 @@ void count_pos(int *arr, int n) {
         ++count[arr[i]];
     }
 
+    /*@
+    @   loop assigns count[0..UPPER_LIMIT], i;
+    @   loop variant n - count[i];   
+    @*/
     for (i = 1; i <= UPPER_LIMIT; ++i) {
         count[i] += count[i - 1];
     }
-    
+
     for (i = 0; i < UPPER_LIMIT; ++i) {
         for (j = count[i]; j < count[i + 1]; ++j) {
             arr[j] = i;
@@ -47,4 +50,22 @@ void count_pos(int *arr, int n) {
     for (j = count[UPPER_LIMIT]; j < n; ++j) {
         arr[j] = UPPER_LIMIT;
     }
+}
+
+#include <stdio.h>
+
+int main(){
+    int arr[5];
+    arr[0] = 2;
+    arr[1] = 3;
+    arr[2] = 1;
+    arr[3] = 1;
+    arr[4] = 5;
+
+    count_pos(&arr[0], 5);
+
+    for(int i = 0; i < 5; ++i){
+        printf("%d\n", arr[i]);
+    }
+    return 0;
 }
